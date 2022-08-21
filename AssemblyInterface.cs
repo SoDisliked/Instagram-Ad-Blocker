be6b398b027ced4d0f900bc4807ff19aadcc5041
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-08-21 15:39:17 +0300

    Add files via upload

---
+++ AssemblyInterface.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// This ad-blocker intends to block ads into Instagram, both into the feed but also into stories.
+// All credits go to SoDisliked on GitHub.
+[assembly: AssemblyTitle("InstagramAdBlocker")]
+[assembly: AssemblyDescription("This AdBlocker has as main intention to block ads on Instagram through Firefox.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoDisliked")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Every copyright attempt may be punishable.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
