41ef76c6706c6bdba3218ea6c7d4b2c8705b96ac
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-08-21 15:39:01 +0300

    Add files via upload

---
+++ Web Api v2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIAdBlocker.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string { } { "value1", "value2" };
+    }
+        // GET api/values/S
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+}
