f4c3d2ddab09700f74408f61377448033d377f6d
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-08-23 00:21:58 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramAdBlocker
+{
+    public static class AdBlocker
+    {
+        public static String VERSION = "";
+        public static String COMPANY = "SoDisliked";
+        public static String PRODUCT = "InstagramAdBlocker";
+        public static String PRODUCTIMGPATH = "";
+        public static String ICONPATH = "InstagramAdBlocker";
+        public static String DefaultBlockPage = "";
+
+       
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// Main information about the AdBlocker is included through the attributes
+// coming. 
+[assembly: AssemblyTitle("InstagramAdBlocker")]
+[assembly: AssemblyDescription("This is an AdBlocker operating in Firefox that automatically blocks ads on Instagram")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Instagram AdBlocker")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
